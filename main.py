b849f32f7cc3b45d38b7ea24db2fd3e0f5e16f78
Daniil-12-24 <smokequitz@gmail.com>
2023-01-28 19:55:22 +0000

    Getting data from the server, via requests and api's.

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
-days = st.slider('Forecast days', min_value=1, max_value=7,
+days = st.slider('Forecast days', min_value=1, max_value=5,
                  help='Select amount of days to Forecast')
 option = st.selectbox('Select data to view',
                       ('Temperature', 'Sky'))
 st.subheader(f'{option} for the next {days} days in {place}')
 
 
-def get_data(days):
-    dates = ['2023-01-25', '2023-01-26', '2023-01-27']
-    temperatures = [5, 6, -1]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+data = get_data(place, days, option)
 
 
 date, temperature = get_data(days)
